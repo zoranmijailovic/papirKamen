3e7c458a6e582dcc8e41faba0343c333ca521d80
zoranmijailovic <zoki.882001@gmail.com>
2023-12-12 14:47:33 +0100

    nova izmena

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const choices = ["rock", "paper", "scissors"];
 
+let btns = document.querySelectorAll('button');
+let result = document.querySelector('.rezultat');
+let playerBoard = document.querySelector('.playerScoreBoard');
+let compBoard = document.querySelector('.compScoreBoard');
+
+
+
 function getComputerChoice(){
   let izbor = choices[randomIndex()];
   return izbor;
@@ -8,24 +15,42 @@ function getComputerChoice(){
 function randomIndex(){
   return Math.floor(Math.random() * choices.length);
 }
+// this one is computer choice
 
-function playGame(){
-  const playerChoice = prompt("chose your weapon");
-  const computerChoice = getComputerChoice();
-  console.log(computerChoice);
-  
 
-  if(playerChoice === computerChoice){
-    return "its a tie";
-  }else if((playerChoice == "rock" && computerChoice == "scissors") ||
-  (playerChoice == "paper" && computerChoice == "rock") ||
-  (playerChoice == "scissors" && computerChoice == "paper")){
-    return "you won!";
-  }else{
-    return "you lose";
-  }
-}
-console.log(playGame());
+let computerScore = 0;
+let playerScore = 0;
+btns.forEach(function(btn){
+    btn.addEventListener('click', () => {
+    const playerChoice = btn.innerText;
+    const computerChoice = getComputerChoice();
+    console.log(computerChoice)
+
+    if(playerChoice === computerChoice){
+      result.textContent = "nereseno";
+      
+    }else if((playerChoice == "rock" && computerChoice == "scissors") ||
+    (playerChoice == "paper" && computerChoice == "rock") ||
+    (playerChoice == "scissors" && computerChoice == "paper")){
+      result.textContent = 'pobedili ste';
+      playerScore++;
+      
+      playerBoard.innerText = playerScore;
+      
+      
+    }else{
+      result.textContent = "izgubili ste";
+      computerScore++;
+      compBoard.textContent = computerScore;
+      
+      
+    }
+    
+  });
+});
+
+
+
 
 
 
